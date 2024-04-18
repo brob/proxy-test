269a1f94c723688fcba0fcd93781e8e09e9ea18d
Bryan Robinson <bryanlrobinson@gmail.com>
2024-04-18 08:48:55 -0400

    Updates proxy to new live site

--- next.config.js
+++ next.config.js
@@ -2,9 +2,14 @@
 const nextConfig = {
     async rewrites() {
         return [
+            {
+                source: '/_astro/:match*',
+                destination: 'http://localhost:4321/_astro/:match*',
+            },
           {
             source: '/docs/:match*',
-            destination: 'https://studio-docs.hygraph.com/:match*',
+            destination: 'https://starlight-proxy-test.vercel.app/docs/:match*/',
+            
           },
         ];
       }
